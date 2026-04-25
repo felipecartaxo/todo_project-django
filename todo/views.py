cd6e4d52c6709a8a4ef24c04148d4d64731f56aa
felipecartaxo <felipecartaxo1@gmail.com>
2026-04-25 19:34:47 -0300

    feat: cria metodo para criar tarefa

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
 
-# Create your views here.
+def todo_list(request): 
+    todos = Todo.objects.all()
+
+    if request.method == "POST":
+        # cria tarefa
+        if "add_task" in request.POST:
+            title = request.POST.get("title")
+            if title:
+                Todo.objects.create(title=title)
+            return redirect("todo_list")
+        
+    
